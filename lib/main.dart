2507e7289fff008bc879788e9b7e97fb098e436c
Carolina Rosatto <carolrosatto@gmail.com>
2025-10-31 11:49:44 -0300

    feat: adds firebase services

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mtg_collect/firebase_options.dart';
 import 'package:mtg_collect/repositories/card_repository.dart';
-import 'package:mtg_collect/services/card_service.dart';
+import 'package:mtg_collect/core/services/api_service.dart';
 import 'package:mtg_collect/viewmodels/card_viewmodel.dart';
 import 'package:mtg_collect/views/widgets/card_list_widget.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  final apiService = CardService();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  print('Firebase iniciado com sucesso ✅');
+
+  final apiService = ApiService();
   final repository = CardRepository(apiService);
 
   runApp(
